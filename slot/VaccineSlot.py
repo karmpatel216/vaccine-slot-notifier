fa519de711a070662ca73b715d4248b7120dfe54
Karm Patel <karmasmart216@gmail.com>
2021-05-06 23:46:40 +0530

    proxy

--- slot/VaccineSlot.py
+++ slot/VaccineSlot.py
@@ -22,11 +22,15 @@ class VaccineSlot:
 
 
     def get_available_slots(self):
+	proxies = {
+	 "http": "http://14.140.131.82:3128",
+	 "https": "http://14.140.131.82:3128"
+	}
         headers = {
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
         #resp = eval(requests.get(self.url, proxies=urllib.request.getproxies()).text)
         #resp = eval(urllib.request.urlopen(self.url).read().decode('utf-8'))
-        resp = eval(requests.get(self.url, headers=headers).content.decode('utf-8'))
+        resp = eval(requests.get(self.url, headers=headers).content.decode('utf-8'),proxies=proxies)
         all_centers = resp['centers']
         min_age = self.data['min_age']
         print("age=",min_age)
