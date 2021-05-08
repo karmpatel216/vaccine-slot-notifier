0b39a9696c3441ec0fd25d5efef1fb70fc63e638
karmpatel216 <karmasmart216@gmail.com>
2021-05-08 14:00:11 +0530

    timestamp

--- showUsers.py
+++ showUsers.py
@@ -2,6 +2,7 @@ from app import db, data
 records = data.query.all()
 for ind,each in enumerate(records):
     print(ind+1,end="\t")
+    print(f"{each.timestamp:10s}",end="\t")
     print(f"{each.email:30s}",end="\t")
     print(f"{each.min_age:10s}",end="\t")
     if each.by == "Area":
